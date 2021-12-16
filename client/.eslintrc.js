38293c954d649ba4f95cdd98f63744e8937195f9
Param Siddharth <contact@paramsid.com>
2021-12-16 15:19:55 +0530

    Initialize front-end

---
+++ client/.eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+	'settings': {
+		'react': {
+			'version': 'detect'
+		}
+	},
+	'env': {
+		'browser': true,
+		'node': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 13,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'eol-last': [
+			'error',
+			'never'
+		],
+		'react/jsx-uses-react': 'off',
+		'react/react-in-jsx-scope': 'off'
+	}
+};
